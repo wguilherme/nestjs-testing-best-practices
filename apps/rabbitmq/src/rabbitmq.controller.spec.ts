e21da0971cc158856337d2f965c56a18fad66ca5
Withney Guilherme <wguilhermecriativo@gmail.com>
2024-04-26 14:35:32 -0300

    refactor: unit and e2e with testcontainer

--- apps/rabbitmq/src/rabbitmq.controller.spec.ts
+++ apps/rabbitmq/src/rabbitmq.controller.spec.ts
@@ -14,8 +14,6 @@ import { GenericContainer } from 'testcontainers';
 
 describe('RabbitMQService Controller (e2e)', () => {
   it('Should receive a heartbat message', async () => {
-    //
-
     const rabbitmq = await new GenericContainer('rabbitmq:3-management')
       .withExposedPorts({ container: 5672, host: 5672 })
       .withExposedPorts({ container: 15672, host: 15672 })
@@ -60,5 +58,44 @@ describe('RabbitMQService Controller (e2e)', () => {
     await app.close();
     client.close();
     await rabbitmq.stop();
-  }, 5000000);
+  }, 50000000);
+
+  it('Should receive a heartbat message2', async () => {
+    //
+
+    const pattern = randomUUID();
+
+    class RabbitmqControllerMock extends RabbitmqController {
+      @MessagePattern(pattern)
+      receive(@Ctx() context): string {
+        return super.receive(context);
+      }
+    }
+
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        ClientsModule.register([{ name: 'output', transport: Transport.TCP }]),
+      ],
+      controllers: [RabbitmqControllerMock],
+      providers: [RabbitmqService],
+    }).compile();
+
+    const app: INestApplication = moduleFixture.createNestApplication();
+    app.connectMicroservice({ transport: Transport.TCP });
+
+    await app.startAllMicroservices();
+    app.init();
+
+    const client: ClientProxy = app.get<ClientProxy>('output');
+    await client.connect();
+
+    const payload = { data: 'output' };
+
+    const response = await client.send(pattern, payload).toPromise();
+
+    expect(response).toEqual('New event received');
+
+    await app.close();
+    client.close();
+  });
 });
