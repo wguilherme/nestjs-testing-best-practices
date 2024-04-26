29f357c300850ae28f6359d96039dbae4fe39922
Herberton Candido Souza <herberton@gmail.com>
2024-04-26 14:26:42 -0300

    feat: adiciona testcontainers

--- apps/rabbitmq/src/rabbitmq.controller.spec.ts
+++ apps/rabbitmq/src/rabbitmq.controller.spec.ts
@@ -1,16 +1,41 @@
 import { INestApplication } from '@nestjs/common';
-import { ClientProxy, ClientsModule, Transport } from '@nestjs/microservices';
+import {
+  ClientProxy,
+  ClientsModule,
+  Ctx,
+  MessagePattern,
+  Transport,
+} from '@nestjs/microservices';
 import { Test, TestingModule } from '@nestjs/testing';
-
-import { RabbitmqModule } from './rabbitmq.module';
+import { randomUUID } from 'crypto';
+import { RabbitmqController } from './rabbitmq.controller';
+import { RabbitmqService } from './rabbitmq.service';
+import { GenericContainer } from 'testcontainers';
 
 describe('RabbitMQService Controller (e2e)', () => {
   it('Should receive a heartbat message', async () => {
+    //
+
+    const rabbitmq = await new GenericContainer('rabbitmq:3-management')
+      .withExposedPorts({ container: 5672, host: 5672 })
+      .withExposedPorts({ container: 15672, host: 15672 })
+      .start();
+
+    const pattern = randomUUID();
+
+    class RabbitmqControllerMock extends RabbitmqController {
+      @MessagePattern(pattern)
+      receive(@Ctx() context): string {
+        return super.receive(context);
+      }
+    }
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
-        RabbitmqModule,
         ClientsModule.register([{ name: 'output', transport: Transport.RMQ }]),
       ],
+      controllers: [RabbitmqControllerMock],
+      providers: [RabbitmqService],
     }).compile();
 
     const app: INestApplication = moduleFixture.createNestApplication();
@@ -22,13 +47,18 @@ describe('RabbitMQService Controller (e2e)', () => {
     const client: ClientProxy = app.get<ClientProxy>('output');
     await client.connect();
 
+    // const controller = app.get(RabbitmqController);
+
+    // console.log(controller);
+
     const payload = { data: 'output' };
 
-    const response = await client.send('output', payload).toPromise();
+    const response = await client.send(pattern, payload).toPromise();
 
     expect(response).toEqual('New event received');
 
     await app.close();
     client.close();
-  });
+    await rabbitmq.stop();
+  }, 5000000);
 });
--- apps/rabbitmq/src/rabbitmq.controller.ts
+++ apps/rabbitmq/src/rabbitmq.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
 import { RabbitmqService } from './rabbitmq.service';
-import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class RabbitmqController {
   constructor(private readonly rabbitmqService: RabbitmqService) {}
 
-  @MessagePattern('output')
-  receive(): string {
+  @MessagePattern()
+  receive(@Ctx() context: RmqContext): string {
+    console.log(context);
     const response = this.rabbitmqService.processEvent();
     return response;
   }
